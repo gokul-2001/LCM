47e47f3d55b1228a65c948c763feeb23c3ef4e9f
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2021-07-13 20:00:46 +0530

    Create lcm.c
    finds lcm of 2 numbers

---
+++ lcm.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int lcm(int,int);
+int main()
+{
+	int a,b,result;
+	int prime[100];
+	printf("Enter First Number:");
+	scanf("%d",&a);
+	printf("Enter Second Number:");
+	scanf("%d",&b);
+	result=lcm(a,b);
+	printf("LCM of %d and %d = %d\n",a,b,result);
+	return 0;
+}
+int lcm(int a,int b)
+{
+	static int common=1;
+	if(common%a==0 && common%b==0)
+	{
+		return common;
+	}
+	common++;
+	lcm(a,b);
+	return common;
+}
